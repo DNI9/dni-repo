a6a083c4df1277dd1346395e8f0120d2684c515e
DNI9 <itsr9boi@gmail.com>
2022-01-21 23:56:26 +0530

    update dwm

--- update.sh
+++ update.sh
@@ -2,6 +2,16 @@
 
 set -euo pipefail
 
+# remove old packages
+rm -f ./x86_64/*.pkg.tar.zst*
+
+## Getting packages from package dirs
+pkgs=$(find ../pkgs -type f -name "*.pkg.tar.zst*")
+for x in ${pkgs}; do
+  echo "Copying ${x}"
+	cp "${x}" x86_64/
+done
+
 cd x86_64
 rm -f dni9-repo*
 
